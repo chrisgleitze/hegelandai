923cd8cb471ecb24a760e7294f268cee226b7ff1
Christian Gleitze <connectingdotscoding@gmail.com>
2026-05-24 13:03:53 +0200

    automated dev commit

--- src/components/LiteratureStats.tsx
+++ src/components/LiteratureStats.tsx
@@ -11,7 +11,7 @@ function StatCard({ value, label, href }: Stat) {
   return (
     <Card
       as="li"
-      className="overflow-hidden rounded-2xl border border-zinc-100 bg-white/80 p-6 shadow-sm shadow-zinc-800/5 ring-1 ring-zinc-900/5 transition duration-200 ease-out hover:-translate-y-px hover:shadow-none dark:border-zinc-700/40 dark:bg-zinc-900/40 dark:ring-white/10 dark:hover:shadow-none"
+      className="overflow-hidden rounded-2xl border border-zinc-100 bg-white/80 p-6 shadow-sm shadow-zinc-800/5 ring-1 ring-zinc-900/5 transition duration-200 ease-out hover:-translate-y-px hover:shadow-md hover:shadow-zinc-300/20 dark:border-zinc-700/40 dark:bg-zinc-900/40 dark:ring-white/10 dark:hover:shadow-zinc-950/10"
     >
       <Card.Link href={href}>
         <span className="block text-3xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100">
